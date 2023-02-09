66c56ee29fa13b80a70b4d5eb34cbc154e917be9
eycha <silvlexonst@gmail.com>
2023-02-09 23:07:33 +0900

    object prac

---
+++ src/ch06_1/PersonTest.java
@@ -0,0 +1,12 @@
+package ch06_1;
+
+import ch06.Student;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person tomas = new Person(180,78,"male","Tomas",37);
+        System.out.println(tomas.showPersonInfo());
+
+
+    }
+}
